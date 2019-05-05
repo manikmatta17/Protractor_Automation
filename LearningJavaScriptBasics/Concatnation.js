dc75811b35a024c7607f28d4f00d3cdf02aa8fd0
manikmatta17 <manikmatta17@gmail.com>
2019-05-05 23:24:15 +1000

    Learning JavaScript Basics

---
+++ LearningJavaScriptBasics/Concatnation.js
@@ -0,0 +1,15 @@
+var text ="manik";
+var text1 ="matta";
+console.log(text + text1);
+console.log(text.length);
+console.log(text.charAt(0));
+console.log("My first name is\"Manik\""+ "My Last name is \"Matta\"");
+
+
+var s1= "way";
+var s2= "2";
+var s3 ="Automation";
+console.log(s1+s2+s3);
+console.log(s1+15+20+s2+s3);
+console.log(15+35+s1+s2+s3);
+console.log(s1+(15+35)+s2+s3);
---
+++ LearningJavaScriptBasics/DefiningVariables.js
@@ -0,0 +1,15 @@
+console.log("Hello World!!");
+var i=10;
+console.log(i);
+
+
+var name ="Manik";
+console.log (name);
+
+
+var value =10.10;
+console.log(value);
+
+
+var isCheck =true;
+console.log(isCheck);
